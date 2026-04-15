f721963bd43341595e0499a098470a5fb01bdf68
GCM4 <guiguicarneiro04@gmail.com>
2026-04-15 20:25:45 -0300

    Continuação da Aula 7

---
+++ nodejs-aula-07-CRUD - Copia/SistemaLoja/controllers/PedidoController.js
@@ -0,0 +1,20 @@
+//Importando o Express com ES6 Modules
+import express from 'express';
+// Método Router do Express para criar rotas de aplicação
+const router = express.Router();
+
+import Pedido from '../models/Pedido.js'
+
+router.get("/pedidos",function(req,res){
+    const pedidos = [
+        {numero: "983721931", valor: 1200},
+        {numero: "983721932", valor: 900},
+        {numero: "983721933", valor: 3200},
+        {numero: "983721934", valor: 150}
+    ]
+    res.render("pedidos", {
+        pedidos: pedidos
+    })
+})
+// Exportando o módulo para usá-lo em outro arquivo
+export default router;
